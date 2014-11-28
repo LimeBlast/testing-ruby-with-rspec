f8c14840ca820f9e93ed40ad330bbabd1d024253
Daniel Hollands <daniel.hollands@intercity-uk.com>
2014-11-28 16:12:27 +0000

    Added a context

--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -6,11 +6,16 @@ describe Blog do
     expect(Blog.new).to be_empty
   end
 
-  it 'has a post when initialized with one' do
-    expect(Blog.new(posts: [Post.new])).not_to be_empty
+  context 'with one post' do
+    let(:blog) { Blog.new(posts: [Post.new]) }
+
+    it 'has a post when initialized with one' do
+      expect(blog).not_to be_empty
+    end
+
+    it 'counts the number of posts' do
+      expect(blog.posts_count).to eq 1
+    end
   end
 
-  it 'counts the number of posts' do
-    expect(Blog.new(posts: [Post.new]).posts_count).to eq 1
-  end
 end
